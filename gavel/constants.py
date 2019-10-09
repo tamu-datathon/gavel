e7a9bfb9e2c706fc03c92f5292aa8bd116ab2a75
Chinmay Phulse <phulsechinmay@gmail.com>
2019-10-09 10:14:03 -0500

    Changed email constants and added docker files

--- gavel/constants.py
+++ gavel/constants.py
@@ -13,42 +13,44 @@ SETTING_FALSE = 'false'
 DEFAULT_WELCOME_MESSAGE = '''
 Welcome to Gavel.
 
-**Please read this important message carefully before continuing.**
+This is the judging system we’re using for **TAMU Datathon 2019**. 
+It’s fully automated and randomly assigns you teams to judge, tells you where to go, and tracks the votes you give to teams. 
 
-Gavel is a fully automated expo judging system that both tells you where to go
-and collects your votes.
+Following are the rules and instructions for using Gavel:
 
-The system is based on the model of pairwise comparison. You'll start off by
-looking at a single submission, and then for every submission after that,
-you'll decide whether it's better or worse than the one you looked at
-**immediately beforehand**.
+- The system is based on the model of pairwise comparison. You’ll start off by looking at a single submission, and then for every submission after that, you’ll decide whether it’s better or worse than the one you looked at **immediately beforehand**.
 
-If at any point, you can't find a particular submission, you can click the
-'Skip' button and you will be assigned a new project. **Please don't skip
-unless absolutely necessary.**
+- If at any point, you can't find a particular submission, you can click the ‘Skip’ button and you will be assigned a new project. **Please don’t skip unless absolutely necessary.**
 
-Gavel makes it really simple for you to submit votes, but please think hard
-before you vote. **Once you make a decision, you can't take it back**.
+- Gavel makes it really simple for you to submit votes, but please think hard before you vote. **Once you make a decision, you can’t take it back**.
+
+Click **Done** once you've understood the rules.
 '''.strip()
 
-DEFAULT_EMAIL_SUBJECT = 'Welcome to Gavel!'
+DEFAULT_EMAIL_SUBJECT = 'TAMU Datathon Judging Info'
 
 DEFAULT_EMAIL_BODY = '''
-Hi {name},
+Hi {name}!
 
-Welcome to Gavel, the online expo judging system. This email contains your
-magic link to the judging system.
+Thank you for being a judge for TAMU Datathon 2019!
 
-DO NOT SHARE this email with others, as it contains your personal magic link.
+You'd be judging teams in the Learner track. The judging system works on pairwise judging, you'll get more information about it when you go to the link below.
 
-To access the system, visit {link}.
+Below is the magic link for you to access the judging system. Please DO NOT share this with others, it's your personal link.
 
-Once you're in, please take the time to read the welcome message and
-instructions before continuing.
+To access the judging system, visit this link: {link}.
+
+Please make sure to read the welcome message and instructions before you continue.
+
+Happy judging!
+
+- TAMU Datathon Team
 '''.strip()
 
 DEFAULT_CLOSED_MESSAGE = '''
-The judging system is currently closed. Reload the page to try again.
+The judging system is closed for now.
+
+If you’ve already finished judging all your teams, you’re done!
 '''.strip()
 
 DEFAULT_DISABLED_MESSAGE = '''
@@ -60,7 +62,7 @@ You are currently logged out. Open your magic link to get started.
 '''.strip()
 
 DEFAULT_WAIT_MESSAGE = '''
-Wait for a little bit and reload the page to try again.
+Seems like the system is having issues assigning you new teams. Please reload the page in a minute.
 
-If you've looked at all the projects already, then you're done.
+If you’ve looked at all the teams already, then you’re done judging!
 '''.strip()
